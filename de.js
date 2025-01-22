b635b5ed2f0e42b6a8cb3171b90d8ea0f01cd5be
vladyslav <vlad@ikarustech.com>
2025-01-22 06:56:44 +0200

    Add confirm  delete post description

--- de.js
+++ de.js
@@ -243,6 +243,7 @@ const de = {
 			"When adding new manufacturers, enter only the manufacturer name (e.g., Canon, Meade) in this section.",
 		manufacturer_tip_section_2:
 			"Specific models (like Meade LX200) should be added later through the manufacturer's dedicated page.",
+		delete_post_description: "Are you sure you want to delete this post? This action cannot be undone and all associated comments will also be removed.",
 	},
 	darkLibrary: {
 		title: "Dark Library",
