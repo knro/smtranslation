44ce49c040893db81a03ab343d7edaff10f39e6c
vladyslav <vlad@ikarustech.com>
2025-02-28 06:26:06 +0200

    update ultimate

--- fr.js
+++ fr.js
@@ -1541,7 +1541,7 @@ const fr = {
 		notifications: "Notifications",
 		sounds: "Activer le Son",
 		cloud_storage: "Stockage en ligne",
-		limit_cloud_storage: "Limited Storage. Upgrade to Pro/Ultimate for up to 10GB/100GB storage ",
+		limit_cloud_storage: "Limited Storage. Upgrade to Pro/Ultimate for up to 10GB/1000GB storage ",
 		auto_sync: "Synchronisation automatique",
 		location: "Emplacement",
 		reset_app: "Réinitialiser l'application",
--- ja.js
+++ ja.js
@@ -1564,7 +1564,7 @@ const ja = {
 		notifications: "通知",
 		sound: "サウンド",
 		cloud_storage: "クラウド ストレージ",
-		limit_cloud_storage: "Limited Storage. Upgrade to Pro/Ultimate for up to 10GB/100GB storage ",
+		limit_cloud_storage: "Limited Storage. Upgrade to Pro/Ultimate for up to 10GB/1000GB storage ",
 		auto_sync: "自動同期",
 		location: "場所",
 		unit_system: "ユニットSystem",
